67265006d88d3edab8a95d5da79884e1f365fb95
rolarbear <jasonromine@cox.net>
2013-08-01 15:57:04 -0700

    add functions

--- main.c
+++ main.c
@@ -432,6 +432,7 @@ void idRead(void) {
 	}
 	GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_3, HIGH);
 }
+
 void dummyWrite(int num) {
 	int i;
 	for (i = 0; i < num; i++) {
@@ -439,6 +440,7 @@ void dummyWrite(int num) {
 		SSIDataGet(SSI0_BASE, &ulDummyRx);
 	}
 }
+
 void memRead(void) {
 	GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_3, LOW);
 	SSIDataPut(SSI0_BASE, 0x0B);
@@ -496,6 +498,21 @@ void memWrite(void) {
 	}
 }
 
+void bmp085Calibration()
+{
+  ac1 = bmp085ReadInt(0xAA);
+  ac2 = bmp085ReadInt(0xAC);
+  ac3 = bmp085ReadInt(0xAE);
+  ac4 = bmp085ReadInt(0xB0);
+  ac5 = bmp085ReadInt(0xB2);
+  ac6 = bmp085ReadInt(0xB4);
+  b1 = bmp085ReadInt(0xB6);
+  b2 = bmp085ReadInt(0xB8);
+  mb = bmp085ReadInt(0xBA);
+  mc = bmp085ReadInt(0xBC);
+  md = bmp085ReadInt(0xBE);
+}
+
 int bmp085ReadInt(unsigned char address) {
 	unsigned char msb, lsb;
 
@@ -529,20 +546,90 @@ int bmp085ReadInt(unsigned char address) {
 	return ((msb<<8) | (lsb));
 }
 
-void bmp085Calibration()
+/************/
+// Read the uncompensated temperature value
+unsigned int bmp085ReadUT()
 {
-  ac1 = bmp085ReadInt(0xAA);
-  ac2 = bmp085ReadInt(0xAC);
-  ac3 = bmp085ReadInt(0xAE);
-  ac4 = bmp085ReadInt(0xB0);
-  ac5 = bmp085ReadInt(0xB2);
-  ac6 = bmp085ReadInt(0xB4);
-  b1 = bmp085ReadInt(0xB6);
-  b2 = bmp085ReadInt(0xB8);
-  mb = bmp085ReadInt(0xBA);
-  mc = bmp085ReadInt(0xBC);
-  md = bmp085ReadInt(0xBE);
+  unsigned int ut;
+  
+  // Write 0x2E into Register 0xF4
+  // This requests a temperature reading
+  //Wire.beginTransmission(BMP085_ADDRESS);
+  //Wire.write(0xF4);
+  //Wire.write(0x2E);
+  //Wire.endTransmission();
+  
+  I2CMasterSlaveAddrSet(I2C1_MASTER_BASE, BMP085_ADDRESS, false);
+  I2CMasterDataPut(I2C1_MASTER_BASE, 0xF4);
+  I2CMasterControl(I2C1_MASTER_BASE, I2C_MASTER_CMD_BURST_SEND_START);
+	while (I2CMasterBusy(I2C1_MASTER_BASE))	{
+	}
+	
+  I2CMasterDataPut(I2C1_MASTER_BASE, 0x2E);
+  I2CMasterControl(I2C1_MASTER_BASE, I2C_MASTER_CMD_BURST_SEND_FINISH);
+	while (I2CMasterBusy(I2C1_MASTER_BASE))	{
+	}
+  
+  // Wait at least 4.5ms
+  delay(5);
+  
+  // Read two bytes from registers 0xF6 and 0xF7
+  ut = bmp085ReadInt(0xF6);
+  return ut;
 }
+/************/
+
+/************/
+short bmp085GetTemperature(unsigned int ut)
+{
+  long x1, x2;
+  
+  x1 = (((long)ut - (long)ac6)*(long)ac5) >> 15;
+  x2 = ((long)mc << 11)/(x1 + md);
+  b5 = x1 + x2;
+
+  return ((b5 + 8)>>4);  
+}
+/************/
+
+/************/
+// Calculate pressure given up
+// calibration values must be known
+// b5 is also required so bmp085GetTemperature(...) must be called first.
+// Value returned will be pressure in units of Pa.
+long bmp085GetPressure(unsigned long up)
+{
+  long x1, x2, x3, b3, b6, p;
+  unsigned long b4, b7;
+  
+  b6 = b5 - 4000;
+  // Calculate B3
+  x1 = (b2 * (b6 * b6)>>12)>>11;
+  x2 = (ac2 * b6)>>11;
+  x3 = x1 + x2;
+  b3 = (((((long)ac1)*4 + x3)<<OSS) + 2)>>2;
+  
+  // Calculate B4
+  x1 = (ac3 * b6)>>13;
+  x2 = (b1 * ((b6 * b6)>>12))>>16;
+  x3 = ((x1 + x2) + 2)>>2;
+  b4 = (ac4 * (unsigned long)(x3 + 32768))>>15;
+  
+  b7 = ((unsigned long)(up - b3) * (50000>>OSS));
+  if (b7 < 0x80000000)
+    p = (b7<<1)/b4;
+  else
+    p = (b7/b4)<<1;
+    
+  x1 = (p>>8) * (p>>8);
+  x1 = (x1 * 3038)>>16;
+  x2 = (-7357 * p)>>16;
+  p += (x1 + x2 + 3791)>>4;
+  
+  return p;
+}
+/************/
+
 
 void sensorPrint(void) {
 	UARTprintf("Accelerometer:  ");
@@ -607,6 +694,9 @@ int main(void) {
 
 			while (1) {
 				accelRead(ADXL345_ADDRESS);
+				/************/
+				bmp085GetTemperature(bmp085ReadUT());
+				/************/
 				pressRead(BMP085_ADDRESS);
 				if (printUart == true) {
 					sensorPrint();
